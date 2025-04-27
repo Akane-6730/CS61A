c2a2deef5e9f91065f915ff630ca1c1b3a9d11f5
Akane <710105188@qq.com>
2025-04-27 22:19:44 +0800

    finish hw8

---
+++ hw/hw08/hw08/hw08.scm
@@ -0,0 +1,33 @@
+(define (ascending? s)
+  (cond
+   ((null? s) #t)
+   ((null? (cdr s)) #t)
+   ((> (car s) (car (cdr s))) #f)
+   (else (ascending? (cdr s)))))
+
+(define (my-filter pred s)
+  (cond
+   ((null? s) '())
+   ((pred (car s)) (cons (car s) (my-filter pred (cdr s))))
+   (else (my-filter pred (cdr s)))))
+
+(define (interleave lst1 lst2)
+  (cond
+   ((null? lst1) lst2)
+   ((null? lst2) lst1)
+   (else (cons (car lst1)
+               (cons (car lst2)
+                     (interleave (cdr lst1) (cdr lst2)))))))
+
+;; A prettier implementation
+(define (interleave lst1 lst2)
+  (cond
+   ((null? lst1) lst2)
+   ((null? lst2) lst1)
+   (else (cons (car lst1) (interleave lst2 (cdr lst1))))))
+
+(define (no-repeats s)
+  (if (null? s) s
+      (cons (car s)
+            (no-repeats
+             (filter (lambda (x) (not (equal? x (car s)))) (cdr s))))))
